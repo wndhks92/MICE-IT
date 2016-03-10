22e51e294c875ff1e5b15de7e90d49944f48bc86
wndhks92 <wndhks92@naver.com>
2016-03-10 17:14:36 +0900

    test1 commit

---
+++ test1/app.js
@@ -0,0 +1,21 @@
+var str="hello world"
+console.log(str);
+console.log(1+2);
+console.log("a"+"b");
+console.log(1+3==4);
+
+var man={
+  name:'jany',
+  age:26,
+  hobby:'cooking'
+}
+
+console.log(man);
+console.log(man.age);
+console.log(man.name);
+
+if(man.name !=="jany"){
+  console.log("who");
+}
+else{console.log("hello")
+}
